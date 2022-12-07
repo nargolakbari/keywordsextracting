06ec3f37479d8cda38b54ab6528751b4c4be1700
nargol <asf@gmail.com>
2022-12-07 12:14:22 +0100

    after naming file

--- keywords.py
+++ keywords.py
@@ -21,18 +21,17 @@ from PIL import Image
 root=tk.Tk()
 root.withdraw()
 file_path=filedialog.askopenfilename()
-print(os.path.splitext(file_path)[0])
-myfilename = print(os.path.splitext(file_path)[0])
-#print(myfilename)  
-#"D:\ISMRM 2023 EPI ghost_v3.docx"
-#input("press any key to exit")
+old_name=file_path
+myfile1= os.path.basename(file_path).split('/')[-1]
+docfilename=myfile1.split(".")[0]  #we obtain the name of file
+
 MY_TEXT = docx2txt.process(file_path)  # convert docx to text
 
 with open("D:\TFL pCASL 3D abstract V3v1.txt", "w", encoding='utf-8') as text_file:  # write text to  a txt file
     print(MY_TEXT, file=text_file)
 
 # word_tokenize accepts
-# a string as an input, not a file.
+# remove stopword include : in/ the / on / verbs/....
 stop_words = set(stopwords.words('english'))  # remove stopwords
 file1 = open("D:\TFL pCASL 3D abstract V3v1.txt", encoding='utf-8')
 
@@ -50,15 +49,14 @@ document_text = open('D:\TFL pCASL 3D abstract V3v12.txt', 'r', encoding='utf-8'
 text_string = document_text.read().lower()
 match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-for word in match_pattern:
+for word in match_pattern:# extract words and their ferequencies 
     count = frequency.get(word, 0)
     frequency[word] = count + 1
 
 frequency_list = frequency.keys()
 wordlist = []
 frequencylist = []
-# for words in frequency_list:
-#    print(words, frequency[words])
+
 
 keys = list(frequency.keys())
 print(keys[1])
@@ -71,7 +69,11 @@ dict = {'word': myval, 'frequency': myvals}
 df = pd.DataFrame(dict)
 dfsorted = df.sort_values(by='frequency', ascending=False)
 # saving the dataframe
-dfsorted.to_csv('D:\GFG.csv')
+#dfsorted.to_csv('D:\GFG.csv')
+folder = "D:\\result"
+#source = 'D:\GFG21.csv'
+destination = folder + '\\'+docfilename +  ".csv"
+dfsorted.to_csv(destination)
 
 #____________________________________________________________________________
 
@@ -88,6 +90,9 @@ df = pd.DataFrame(dict)
 # saving the dataframe 
 df.to_csv('D:\GFG2.csv') 
 #print(keyword_extracted)
+destination1 = folder + '\\'+ docfilename + "_phrase1" +".csv"
+df.to_csv(destination1)
+
 #_______________
 #open text file in read mode
 text_file = open("D:\TFL pCASL 3D abstract V3v12.txt", "r", encoding='utf-8')
@@ -100,11 +105,17 @@ keyword_extracted = rake_nltk_var.get_ranked_phrases()[0:30]
 keyword_extracted
 dict1 = {'word': keyword_extracted}  
        
-df1 = pd.DataFrame(dict1) 
+df2 = pd.DataFrame(dict1) 
     
 # saving the dataframe 
-df1.to_csv('D:\GFG21.csv') 
-#print(keyword_extracted)
+#df1.to_csv('D:\GFG21.csv') 
+destination2 = folder + '\\'+ docfilename + "_phrase2" +".csv"
+df2.to_csv(destination2)
+document_text.close()
+file1.close()
+text_file.close()
+os.remove("D:\TFL pCASL 3D abstract V3v1.txt")
+os.remove("D:\TFL pCASL 3D abstract V3v12.txt")
 #____________________________________________________________________________
 
 
